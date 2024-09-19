d0d5e98ce75ec25329b87f7700661e37dc2e42cb
Alana <rcclewu@gmail.com>
2024-09-19 11:15:23 -0700

    exercise 15 completed

--- exercises/ex07.js
+++ exercises/ex07.js
@@ -21,3 +21,39 @@ In 2015, a person with a Bachelor's degree earned an average of $59,124/year.
 TIP: To print out the average salary with commas (i.e. 59,124), use the toLocaleString() method and pass it the locale "en-US". For example, salary.toLocaleString("en-US"). 
 
 */
+
+const education = "a high school diploma"
+let salary = null
+
+switch(education) {
+
+    case "no high school diploma":
+        salary = 25636
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break;
+
+    case "a high school diploma":
+        salary = 35256
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break;
+
+    case "a Bachelor's degree":
+        salary = 59124
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break; 
+        
+    case "a Master's degree":
+        salary = 69732
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break; 
+
+    case "a Professional degree":
+        salary = 89960
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break;
+
+    case "a Doctoral degree":
+        salary = 84396
+        console.log(`In 2015, a person with ${education} earned an average of $${salary.toLocaleString("en-US")}/year.`)
+        break;              
+}
--- exercises/ex15.js
+++ exercises/ex15.js
@@ -7,3 +7,9 @@ currentYear – a number representing the current year
 The ageCalculator function should return a string explaining how old the person is. For example, if we called ageCalculator("John", 1983, 2015);, the return value should be as follows.
 "John is 32 years old."
 */
+
+ageCalculator ("John", 1983, 2024)
+function ageCalculator(name, birthYear, currentYear){
+    let age = currentYear - birthYear
+    console.log(`${name} is ${age} years old.`)
+}
