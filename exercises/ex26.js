30fbfb20d6401ce82098671df1f309b97458d57d
Alana <rcclewu@gmail.com>
2024-09-22 18:00:37 -0700

    exercise 26-29 completed

--- exercises/ex26.js
+++ exercises/ex26.js
@@ -5,3 +5,9 @@ The Lumberjack - $9.95
 eggs, sausage, toast, hashbrowns, pancakes
 The object should contain properties for the name, price, and ingredients.
 */
+const menu = {
+    name : "Lumberjack",
+    price : 9.95,
+    ingredient : ["eggs", "suasage", "toast", "hashbrowns", "pancakes"],
+}
+console.log(menu)
--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,31 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+    name :"Alana",
+    numOfFriends : 3,
+    arrayMessage : ["Hi, nice to meet you!"],
+    postMessage : function(message) {
+        this.arrayMessage[this.arrayMessage.length] = message
+        return this.arrayMessage
+    },
+    deleteMessage : function (index) {
+        var arr = this.arrayMessage
+        arr.splice(index,1)
+        return arr
+    
+    },
+    addFriends : function() {
+       return ++ this.numOfFriends 
+    },
+    removeFriends : function() {
+        return --this.numOfFriends 
+    }
+
+}
+
+console.log(facebookProfile.postMessage("see you later"))
+console.log(facebookProfile.postMessage("okok"))
+console.log(facebookProfile.deleteMessage(1))
+
